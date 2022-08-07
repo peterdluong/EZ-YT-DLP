0ef51ea459f9ef3b1811d37f91b088d9e4304b93
Peter Luong <real.peter.luong@gmail.com>
2022-08-07 14:28:24 -0500

    added gitignore, fixed on windows, minor improvements

--- py-yt-dlp.py
+++ py-yt-dlp.py
@@ -1,19 +1,24 @@
+from distutils.log import error
 import os
+import sys
 
-dl_format = input("What format do you want to download in? Options are: audio, video\n")
+dl_format = input("What format do you want to download in? Options are: audio, video\n").lower()
 
 # audio downloads
 if (dl_format == "audio" or "a"):
-    audio_format = input("What audio format?\n")
-
-    if (audio_format == "3" or "MP3"):
+    audio_format = input("What audio format?\n").lower()
+    if (audio_format == "3" or "mp3"):
         audio_format = "mp3"
-
 # video downloads
-else:
-    print("you suck\n")
+elif (dl_format == "video" or "v"):
+    error("you suck\n")
     exit()
 
 YT_link = input("Paste the YouTube link\n")
 
-home_dir = os.system('yt-dlp -x --audio-format '+audio_format+' "'+YT_link+'"')
+# different command depending on operating system
+if sys.platform == 'win32':
+    os.system('cd C:\ytdl')
+    os.system('yt-dlp -x --audio-format '+audio_format+' --paths "Downloads/" "'+YT_link+'"')
+elif sys.platform == 'darwin':
+    print('hi')
